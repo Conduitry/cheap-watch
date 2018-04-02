3c225c4bce50efccb75cf4f6d36d69d89d9f35d0
Conduitry <git@fur.army>
2018-04-02 16:51:27 -0400

    more stuff

--- CheapWatch.js
+++ CheapWatch.js
@@ -10,6 +10,8 @@ const _stats = Symbol('_stats');
 const _timeouts = Symbol('_timeouts');
 const _queue = Symbol('_queue');
 const _isProcessing = Symbol('_isProcessing');
+const _isInited = Symbol('_isInited');
+const _isClosed = Symbol('_isClosed');
 const _recurse = Symbol('_recurse');
 const _handle = Symbol('_handle');
 const _enqueue = Symbol('_enqueue');
@@ -17,25 +19,56 @@ const _enqueue = Symbol('_enqueue');
 export default class CheapWatch extends EventEmitter {
 	constructor({ dir, filter, watch = true, debounce = 10 }) {
 		super();
+		// root directory
 		this.dir = dir;
+		// (optonal) function to limit watching to certain directories/files
 		this.filter = filter;
+		// (optional) whether to actually watch for changes, or just report all matching files and their stats
 		this.watch = watch;
+		// (optional) number of milliseconds to use to debounce events from FSWatcher
 		this.debounce = debounce;
-		this[_watchers] = new Map(); // paths of all directories -> FSWatcher instances
-		this[_stats] = new Map(); // paths of all files -> file stats
-		this[_timeouts] = new Map(); // paths of files with pending debounced events -> setTimeout timer ids
-		this[_queue] = []; // queue of pending FSWatcher events to handle
-		this[_isProcessing] = false; // whether some FSWatcher event is currently already in the process of being handled
+		// paths of all directories -> FSWatcher instances
+		this[_watchers] = new Map();
+		// paths of all files -> file stats
+		this[_stats] = new Map();
+		// paths of files with pending debounced events -> setTimeout timer ids
+		this[_timeouts] = new Map();
+		// queue of pending FSWatcher events to handle
+		this[_queue] = [];
+		// whether some FSWatcher event is currently already in the process of being handled
+		this[_isProcessing] = false;
+		// whether init has been called
+		this[_isInited] = false;
+		// whether close has been called
+		this[_isClosed] = false;
 	}
 
 	// recurse directroy, get stats, set up FSWatcher instances
 	// returns array of { path, stats }
 	async init() {
+		if (this[_isInited]) {
+			throw new Error('Cannot init a CheapWatch that has already been inited');
+		}
+		this[_isInited] = true;
 		await this[_recurse](this.dir);
 		return [...this[_stats].entries()].map(([path, stats]) => ({
 			path,
 			stats,
 		}));
+	}
+
+	// close all FSWatchers
+	close() {
+		if (!this[_isInited]) {
+			throw new Error('Cannot close a CheapWatch that has not yet been inited');
+		}
+		if (this[_isClosed]) {
+			throw new Error('Cannot close a CheapWatch that has already been closed');
+		}
+		this[_isClosed] = true;
+		for (const watcher of this[_watchers].values()) {
+			watcher.close();
+		}
 	}
 
 	// recurse a given directory
@@ -92,7 +125,8 @@ export default class CheapWatch extends EventEmitter {
 					this[_stats].set(path, stats);
 					this.emit('+', { path, stats });
 				} else if (stats.isDirectory() && !this[_watchers].has(path)) {
-					// note the new directory: start watching it, and report any files in it
+					// note the new directory
+					// start watching it, and report any files in it
 					await this[_recurse](full);
 					for (const [newPath, stats] of this[_stats].entries()) {
 						if (newPath.startsWith(path + '/')) {
@@ -107,7 +141,8 @@ export default class CheapWatch extends EventEmitter {
 					this[_stats].delete(path);
 					this.emit('-', { path });
 				} else if (this[_watchers].has(path)) {
-					// note the deleted directory: stop watching it, and report any files that were in it
+					// note the deleted directory
+					// stop watching it, and report any files that were in it
 					for (const old of this[_watchers].keys()) {
 						if (old === path || old.startsWith(path + '/')) {
 							this[_watchers].get(old).close();
