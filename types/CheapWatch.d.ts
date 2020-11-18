e2484101e8aba6fa0d3602bd9fd615fa4d94782f
Luke Edwards <luke.edwards05@gmail.com>
2020-11-18 08:49:51 -0800

    fix(types): use `type` import (#5)

--- types/CheapWatch.d.ts
+++ types/CheapWatch.d.ts
@@ -1,17 +1,17 @@
-import * as EventEmitter from 'events';
-import * as fs from 'fs';
+import type { Stats } from 'fs';
+import type { EventEmitter } from 'events';
 
 export default class CheapWatch extends EventEmitter {
 	dir: string;
 	filter?: Filter;
 	watch: boolean;
 	debounce: number;
-	paths: Map<string, fs.Stats>;
+	paths: Map<string, Stats>;
 	constructor(data: object);
 	init(): Promise<void>;
 	close(): void;
 }
 
 interface Filter {
-	(file: { path: string; stats: fs.Stats }): Promise<boolean>;
+	(file: { path: string; stats: Stats }): Promise<boolean>;
 }
